45c9081f133e721907d2ae855a583de81b15f740
Croft-Ritete Shailo <scroftritete1@woolworths.com.au>
2022-05-24 08:11:14 +1000

    added new C# files

---
+++ MineSweeper/Grid.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineSweeper
+{
+    public class Grid
+    {
+        // Three-dimensional array.
+        public int[,,] array3D = new int[,,] { { { 1, 2, 3 }, { 4, 5, 6 } }, { { 7, 8, 9 }, { 10, 11, 12 } } };
+
+
+        public Grid(int[] array3D)
+        {
+     
+        }
+    }
+
+    
+
+}
---
+++ MineSweeper/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MineSweeper
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to MineSweeper!!");
+        Console.WriteLine("Choose your X and Y spaces to start uncovering the tiles..");
+        Console.WriteLine("But be careful not to step on any mines on the way");
+            
+        }
+
+
+}
+}
